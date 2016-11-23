5a2118785f469eb465d15ef32b8c821d1f5d7deb
vahuir <v.ahuir@gmail.com>
2016-11-24 00:40:30 +0100

    Initial commit

---
+++ web/index.php
@@ -0,0 +1,224 @@
+﻿<?php
+$trackId = "11JlOByl09xCxfx5CeSHgB";
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Spotify Manu y Mónica</title>
+
+	<!-- jQuery, Bootstrap, Font Awesome, AngularJS, Firebase, AngularFire -->
+	<script src="https://code.jquery.com/jquery.min.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.2/angular.min.js"></script>
+	<script src="https://cdn.firebase.com/js/client/2.2.1/firebase.js"></script>
+	<script src="https://cdn.firebase.com/libs/angularfire/1.0.0/angularfire.min.js"></script>
+
+	<link rel="stylesheet" href="static/css/styles.css" />
+	<script type="text/javascript" src="static/js/main.js"></script>
+</head>
+
+<body>
+
+	<div ng-app="myApp" ng-controller="mainCtrl">
+
+		<!--Sign up modal for new users-->
+
+		<div class="modal fade" id="signupModal" tabindex="-1">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+						<h4 class="modal-title" id="signupModalTitle">Sign Up</h4>
+					</div>
+					<div class="modal-body">
+						<form name="signupForm" ng-show="!userId" novalidate>
+							<div class="form-group">
+								<label for="signupEmail">Email</label>
+								<input class="form-control" id="signupEmail" ng-model="email" name="email" type="email" placeholder="Give an email">
+								<p class="help-block" ng-show="signupForm.email.$error.email">Not an email!</p>
+							</div>
+							<div class="form-group">
+								<label for="signupPassword">Password</label>
+								<input class="form-control" name="password" ng-model="password" id="signupPassword" type="password" placeholder="Give a password">
+							</div>
+							<button id="signupSubmitButton" ng-click="signUp()" ng-disabled="myForm.$invalid" type="submit" class="btn btn-primary">Sign Up</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!--Log in modal for existing users
+
+		<div class="modal fade" id="loginModal" tabindex="-1">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+						<h4 class="modal-title" id="loginModalTitle">Log In</h4>
+					</div>
+					<div class="modal-body">
+						<form name="loginForm">
+							<div class="form-group">
+								<label for="loginEmail">Email</label>
+								<input class="form-control" id="loginEmail" ng-model="email" type="email" placeholder="Give your email">
+								<p class="help-block" ng-show="loginForm.email.$error.email">Not an email!</p>
+							</div>
+							<div class="form-group">
+								<label for="loginPasword">Password</label>
+								<input class="form-control" ng-model="password" name="password" id="loginPasword" type="password" placeholder="Give your password">
+							</div>
+							<button id="loginSubmitButton" ng-click="signIn()" ng-disabled="myForm.email.$invalid || myForm.password.$invalid" type="submit"
+							class="btn btn-primary">Log In</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div> -->
+
+		<!--Navbar for the page-->
+
+		<nav class="navbar navbar-default">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<a href="#" class="navbar-brand">Boda M&M</a>
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-navbar" aria-expanded="false">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+				</div>
+				<div class="collapse navbar-collapse" id="main-navbar">
+					<ul class="nav navbar-nav">
+						<li class="navbar_component"><a href="#queryTrackSection">Buscar una canción</a></li>
+						<!-- <li class="navbar_component"><a href="#trackPlayerSection">Play a track</a></li> -->
+						<li class="navbar_component"><a href="#trackListSection">Lista más votada</a></li>
+					</ul>
+					<div id="navbarButtonGroup" class="pull-right">
+
+						<button id="signupButton" class="btn btn-primary" data-toggle="modal" data-target="#signupModal">Sign Up</button>
+
+						<button id="loginButton" class="btn btn-primary" data-toggle="modal" data-target="#loginModal">Log In</button>
+
+						<button id="logoutButton" class="btn btn-primary">Log Out</button>
+					</div>
+				</div>
+			</div>
+		</nav>
+
+		<!--Welcome-->
+		<section id="welcomeSection" class="jumbotron container">
+			<h1 id="welcomeTitle">Boda M&M</h1>
+			<h3 id="welcomeSubtitle">Elige tu la música...para este día tan especial</h3>
+			<!--
+		<h2 id="welcomeActionTitle">get started</h2>
+		-->
+		</section>
+
+		<!--Query Tracks-->
+		<section id="queryTrackSection" class="jumbotron container">
+			<h2>Busca tu cancion preferida</h2>
+
+			<div class="row">
+				<div class="col-xs-12 col-md-6 col-md-offset-3">
+					<form id="trackQueryForm" name="queryForm">
+						<div class="form-group">
+							<!-- <label for="queryInput">Search for a track:</label> -->
+							<input id="queryInput" name="trackSearch" ng-model="track" class="form-control" placeholder="Marry you">
+						</div>
+						<button id="queryButton" ng-disabled="queryForm.trackSearch.$invalid" ng-click="searchTracks()" type="submit" class="btn btn-default">Buscar</button>
+					</form>
+				</div>
+			</div>
+		</section>
+
+		<!--Track Query Results-->
+		<section ng-show="queryTracks.length>0" id="queryResultsSection" class="jumbotron container">
+			<h3>Results:</h3>
+
+			<div id="queryResultsList">
+				<table class="table">
+					<thead>
+						<tr>
+							<th>Escuchar</th>
+							<th>Título</th>
+							<th>Artista</th>
+					<!--		<th>Duración (s)</th> -->
+							<th>Añadir a la lista</th>
+						</tr>
+					</thead>
+					<tbody ng-repeat="track in queryTracks">
+						<tr>
+							<td title="{{track.name}}">
+								<button class="button-fa" id="{{track.id}}" ng-click="preview(track.preview_url, track.id)"><i class="fa fa-play-circle"></i></button>
+							</td>
+							<td>{{track.name}}</td>
+							<td>{{track.artists[0].name}}</td>
+							<!-- <td>{{track.duration_ms}}</td> -->
+							<td>
+								<button ng-click="addTrack(track.id, track.name, track.artists[0].name, track.duration_ms, track.preview_url)" class="addButton btn btn-success">Add</button>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</section>
+
+		<!--Track Player-->
+		<section ng-show="playlist.length>0" id="trackPlayerSection" class="jumbotron container">
+			<h2>Escuchar The Playlist</h2>
+
+			<div id="previewPlaylistWrapper">
+				<iframe src="https://embed.spotify.com/?uri=spotify:track:<?php echo $trackId?>" width="300" height="300" frameborder="0"
+				allowtransparency="true"></iframe>
+			</div>
+
+		</section>
+
+		<!--Track List-->
+		<section ng-show="playlist.length>0" id="trackListSection" class="jumbotron container">
+			<h2>Playlist</h2>
+
+			<div id="trackPlaylistWrapper">
+				<table id="trackPlaylistTable" class="table">
+					<thead>
+						<tr>
+							<th>Escuchar</th>
+							<th>Título</th>
+							<th>Artista</th>
+							<!-- <th>Duration (ms)</th> -->
+							<th>Nº de votos</th>
+							<th>Votar + 1</th>
+						</tr>
+					</thead>
+					<tbody ng-repeat="track in playlist | orderBy : '-upvotes'">
+						<tr class="playlistTrackRow" id="{{track.$id}}" ">
+							<td title="{{track.name}} "><button class="previewButton" ng-click="previewPlaylistTrack(track.id)">Escuchar</button></td>
+							<td>{{track.name}}</td>
+							<td>{{track.artist}}</td>
+						<!--	<td>{{track.duration}}</td> -->
+							<td class="upvotesColValue">{{track.upvotes}}</td>
+							<td><button class="button-fa" ng-click="upvote(track.$id)"><i class="fa fa-chevron-circle-up"></i></button></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</section>
+		
+	</div>
+	
+<!--
+	<footer id="mainFooter" class="jumbotron container">
+
+	</footer>
+-->
+</body>
+
+</html>
---
+++ web/static/js/main.js
@@ -0,0 +1,195 @@
+var trackQueryData;
+var currentPreview;
+
+var baseUrl = 'https://api.spotify.com/v1/search?type=track&limit=10&market=ES&query=';
+var myApp = angular.module('myApp', ["firebase"]);
+
+var mainCtrl = myApp.controller('mainCtrl', function ($scope, $http, $firebaseArray, $firebaseObject, $firebaseAuth) {
+
+	var ref = new Firebase('https://boda-manuymonica.firebaseio.com');
+
+	var tracksRef = ref.child('tracks');
+	var usersRef = ref.child('users');
+
+	$scope.users = $firebaseObject(usersRef);
+	$scope.authObj = $firebaseAuth(ref);
+
+	$scope.authData = $scope.authObj.$getAuth();
+
+	/*
+	$scope.authObj.$onAuth(function (authDataOriginal) {
+		$scope.authData = authDataOriginal;
+	});
+
+	*/
+	if ($scope.authData) {
+		$scope.userId = $scope.authData.uid;
+
+		$("#loginButton").css('display', 'none');
+		$("#logoutButton").css('display', 'inline');
+		$("#signupButton").css('display', 'none');
+	}
+	else {
+		$("#loginButton").css('display', 'inline');
+		$("#logoutButton").css('display', 'none');
+	}
+
+	$scope.playlist = $firebaseArray(tracksRef);
+
+	$scope.audioObject = {}
+	$scope.currentTrackAudio = {}
+	
+	//Sign In
+	$scope.signUp = function () {
+
+		$scope.authObj.$createUser({
+			email: $scope.email,
+			password: $scope.password,
+		})
+			.then($scope.logIn)
+
+			.then(function (authData) {
+				$scope.userId = authData.uid;
+				$scope.users.$save()
+			})
+
+			.then($("#signupModal").modal('hide'))
+
+			.catch(function (error) {
+				console.error("Error: ", error);
+			});
+	}
+	
+	//Sign In
+	$scope.signIn = function () {
+		$scope.logIn().then(function (authData) {
+			$scope.userId = authData.uid;
+		})
+	}
+	
+	//Log In
+	$scope.logIn = function () {
+		return $scope.authObj.$authWithPassword({
+			email: $scope.email,
+			password: $scope.password
+		})
+	}
+	
+	//Log Out
+	$scope.logOut = function () {
+		$scope.authObj.$unauth()
+		$scope.userId = false
+	}
+	
+	//Get Tracks From Query
+	$scope.searchTracks = function () {
+		$http.get(baseUrl + $scope.track).success(function (response) {
+			trackQueryData = $scope.queryTracks = response.tracks.items
+			console.log(baseUrl + $scope.track)
+		})
+	}
+	
+	//Play Track
+	$scope.preview = function (song, id) {
+		if ($scope.currentSong == song) {
+			$scope.audioObject.pause()
+
+			$("#" + id).html('<i class="fa fa-play-circle"></i>')
+
+			$scope.currentSong = false
+			return
+		}
+		else {
+			if ($scope.audioObject.pause != undefined) $scope.audioObject.pause()
+
+			if (currentPreview != id) {
+				$("#" + currentPreview).html('<i class="fa fa-play-circle"></i>')
+			}
+
+			$("#" + id).html('<i class="fa fa-stop"></i>')
+
+			currentPreview = id
+
+			$scope.audioObject = new Audio(song);
+			$scope.audioObject.play()
+			$scope.currentSong = song
+		}
+	}
+	
+	//Add Track To Playlist
+	$scope.addTrack = function (id, name, artist, duration, preview_url) {
+		console.log("Add " + name + " by " + artist);
+		$scope.playlist.$add({
+			id: id,
+			name: name,
+			artist: artist,
+			duration: duration,
+			preview_url: preview_url,
+			upvotes: 0
+		})
+
+			.then(function () {
+				console.log("Added " + name + " by " + artist);
+			})
+	}
+	
+	//Preview Track From Playlist
+	$scope.previewPlaylistTrack = function (id) {
+		var previewURL = 'https://embed.spotify.com/?uri=spotify:track:' + id;
+
+		var iFrame = $("<iframe width='300' height='300' frameborder='0' allowtransparency='true'></iframe>");
+
+		iFrame.attr('src', previewURL);
+
+		$('#previewPlaylistWrapper').html(iFrame);
+	}
+	
+	//Play Track From Playlist
+	$scope.playTrack = function (id, name, artist, image_url, preview_url) {
+		if ($scope.currentTrackPlaying == song) {
+			$scope.currentTrackAudio.pause()
+
+		}
+		else {
+			if ($scope.audioObject.pause != undefined) $scope.audioObject.pause()
+			$("#" + currentPreview).html('<i class="fa fa-play-circle"></i>')
+
+			$scope.currentTrackAudio = new Audio(preview_url);
+			$scope.currentTrackAudio.play();
+			$scope.currentTrackPlaying = preview_url;
+		}
+	}
+	
+	//Upvote A Track
+	$scope.upvote = function (id) {
+		var tempTrackRef = tracksRef.child(id);
+		var queryRef = tempTrackRef.child('upvotes');
+
+		if (queryRef != null) {
+			queryRef.transaction(function (upvoteNumber) {
+				return upvoteNumber + 1;
+			},
+				function (error, committed, snapshot) {
+					if (error) {
+						alert(error)
+						alert("Upvote transaction failed abnormally!");
+					}
+					else if (!committed) {
+						alert("Upvote transaction aborted!");
+					}
+					else {
+						console.log("Upvote transaction committed");
+					}
+				}
+				);
+		}
+		else {
+			alert("Accessing Upvote Ref Failed!");
+		}
+	}
+
+});
+
+$('body').tooltip({
+	selector: '[title]'
+});
